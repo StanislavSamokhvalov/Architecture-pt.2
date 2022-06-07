1c9d8cba10b625e0603c1ca58e4aee1e2434ad6f
StanislavSamokhvalov <stanislavsamokhvalov7@gmail.com>
2022-06-07 19:31:39 +0400

    initial commit

---
+++ app/src/main/java/ru/netology/nmedia/repository/PostRemoteMediator.kt
@@ -0,0 +1,108 @@
+package ru.netology.nmedia.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import ru.netology.nmedia.api.ApiService
+import ru.netology.nmedia.dao.PostDao
+import ru.netology.nmedia.dao.PostRemoteKeyDao
+import ru.netology.nmedia.db.AppDb
+import ru.netology.nmedia.entity.PostEntity
+import ru.netology.nmedia.entity.PostRemoteKeyEntity
+import ru.netology.nmedia.entity.toEntity
+import ru.netology.nmedia.error.ApiError
+
+@OptIn(ExperimentalPagingApi::class)
+class PostRemoteMediator(
+    private val service: ApiService,
+    private val db: AppDb,
+    private val postDao: PostDao,
+    private val postRemoteKeyDao: PostRemoteKeyDao,
+) : RemoteMediator<Int, PostEntity>() {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, PostEntity>
+    ): MediatorResult {
+
+//
+//Автоматический PREPEND был отключен (т.е. при scroll'е к первому сверху элементу данные автоматически не подгружались).
+//REFRESH не затирал предыдущий кеш, а добавлял данные сверху, учитывая id последнего поста сверху (соответственно, swipe to refresh должен "добавлять" данные, а не затирать их).
+//APPEND работал в обычном режиме.
+//
+//Убедитесь, что ваше приложение нормально функционирует при следующих сценариях работы:
+//
+//Чистая установка (БД пустая).
+//Запуск с заполненной БД (в БД остались данные с предыдущего запуска).
+
+
+        try {
+            val response = when (loadType) {
+                LoadType.REFRESH -> {
+                    postRemoteKeyDao.max()?.let {
+                        service.getAfter(it, state.config.initialLoadSize)
+                    } ?: service.getLatest(state.config.initialLoadSize)
+                }
+
+                LoadType.PREPEND -> {
+                    return MediatorResult.Success(
+                        endOfPaginationReached = false
+                    )
+                }
+                LoadType.APPEND -> {
+                    val id = postRemoteKeyDao.min() ?: return MediatorResult.Success(
+                        endOfPaginationReached = false
+                    )
+                    service.getBefore(id, state.config.pageSize)
+                }
+            }
+
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(
+                response.code(),
+                response.message(),
+            )
+
+            db.withTransaction {
+                when (loadType) {
+                    LoadType.REFRESH -> {
+                        if (postRemoteKeyDao.isEmpty()) {
+                            postRemoteKeyDao.insert(
+                                listOf(
+                                    PostRemoteKeyEntity(
+                                        type = PostRemoteKeyEntity.KeyType.BEFORE,
+                                        id = body.last().id,
+                                    )
+                                )
+                            )
+                        }
+                    }
+                    LoadType.PREPEND -> {
+//                        postRemoteKeyDao.insert(
+//                            PostRemoteKeyEntity(
+//                                type = PostRemoteKeyEntity.KeyType.AFTER,
+//                                id = body.first().id,
+//                            )
+//                        )
+                    }
+                    LoadType.APPEND -> {
+                        postRemoteKeyDao.insert(
+                            PostRemoteKeyEntity(
+                                type = PostRemoteKeyEntity.KeyType.BEFORE,
+                                id = body.last().id,
+                            )
+                        )
+                    }
+                }
+                postDao.insert(body.toEntity())
+            }
+            return MediatorResult.Success(endOfPaginationReached = body.isEmpty())
+        } catch (e: Exception) {
+            return MediatorResult.Error(e)
+        }
+    }
+}
+
